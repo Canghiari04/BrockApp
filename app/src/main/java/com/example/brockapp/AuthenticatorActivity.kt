6264e8798ad7eac76220e7f2f3ca294e66af5a9b
Canghiari04 <matteo.canghiari@gmail.com>
2024-05-13 14:35:24 +0200

    corrette dipendenze di Gradle

--- app/src/main/java/com/example/brockapp/AuthenticatorActivity.kt
+++ app/src/main/java/com/example/brockapp/AuthenticatorActivity.kt
@@ -1,6 +1,9 @@
 package com.example.ppt
+package com.example.authenticator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import com.example.brockapp.R
 
@@ -8,5 +11,17 @@ class AuthenticatorActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.authenticator_activity)
+
+        findViewById<Button>(R.id.button_auth).setOnClickListener {
+            authenticateBuiltIn()
+        }
+    }
+
+    private fun authenticateBuiltIn() {
+        val username = findViewById<EditText>(R.id.text_username).text.toString()
+        val password = findViewById<EditText>(R.id.text_password).text.toString()
+
+        // I CAN COMMUNICATE THE CREDENTIALS AND THEN RETURN A BOOLEAN VALUE THAT TELL ME IF THEY ARE TRUTHY OR FALSY
+
     }
 }
