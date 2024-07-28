292d91e2c7c697fd971219c6a6a2129ddb96818b
Canghiari04 <matteo.canghiari@gmail.com>
2024-07-28 15:19:58 +0200

    Realizzato singoletto per utente

--- app/src/main/java/com/example/brockapp/User.kt
+++ app/src/main/java/com/example/brockapp/User.kt
@@ -1,4 +1,16 @@
 package com.example.brockapp
 
-class User {
+/**
+ * Singoletto utilizzato per mantenere durante il run dell'applicazione il riferimento ad una sola istanza User.
+ */
+object User {
+    private val instance = User
+
+    var id: Long = 0
+    var username: String = ""
+    var password: String = ""
+
+    fun getInstance() : User {
+        return instance
+    }
 }
