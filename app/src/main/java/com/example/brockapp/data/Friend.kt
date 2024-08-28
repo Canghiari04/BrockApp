f61f7cc3f2c6a80e3267e05b51b6b4761e444605
Canghiari04 <matteo.canghiari@gmail.com>
2024-08-28 18:50:51 +0200

    indentato codice

--- app/src/main/java/com/example/brockapp/data/Friend.kt
+++ app/src/main/java/com/example/brockapp/data/Friend.kt
@@ -1,15 +1,12 @@
 package com.example.brockapp.data
 
-import com.example.brockapp.activity.StillActivity
-import com.example.brockapp.activity.VehicleActivity
-import com.example.brockapp.activity.WalkActivity
 import com.example.brockapp.database.UserStillActivityEntity
 import com.example.brockapp.database.UserVehicleActivityEntity
 import com.example.brockapp.database.UserWalkActivityEntity
 
 data class Friend(
     val username : String,
-    var walkActivities: MutableList<UserWalkActivityEntity> = mutableListOf<UserWalkActivityEntity>(),
-    var vehicleActivities : MutableList<UserVehicleActivityEntity> = mutableListOf<UserVehicleActivityEntity>(),
-    var stillActivities : MutableList<UserStillActivityEntity> = mutableListOf<UserStillActivityEntity>()
+    var walkActivities: MutableList<UserWalkActivityEntity> = mutableListOf(),
+    var stillActivities : MutableList<UserStillActivityEntity> = mutableListOf(),
+    var vehicleActivities : MutableList<UserVehicleActivityEntity> = mutableListOf()
 )
