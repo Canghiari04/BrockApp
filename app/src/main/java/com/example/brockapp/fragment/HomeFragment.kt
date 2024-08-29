fb4abc4059feb5572f279b84eabf71d92d42cf3a
Canghiari04 <matteo.canghiari@gmail.com>
2024-08-29 18:43:05 +0200

    corretta logica view model

--- app/src/main/java/com/example/brockapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/brockapp/fragment/HomeFragment.kt
@@ -9,81 +9,127 @@ import com.example.brockapp.adapter.HomeAdapter
 import com.example.brockapp.viewmodel.ActivitiesViewModel
 import com.example.brockapp.viewmodel.ActivitiesViewModelFactory
 
+import android.util.Log
 import android.os.Bundle
 import android.view.View
 import java.time.LocalDate
+import java.time.DayOfWeek
+import android.widget.Spinner
 import android.widget.TextView
-import kotlinx.coroutines.launch
 import android.widget.ProgressBar
-import kotlinx.coroutines.Dispatchers
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import kotlinx.coroutines.CoroutineScope
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalAdjusters
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.LinearLayoutManager
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
     private lateinit var user: User
-    private lateinit var db: BrockDB
     private lateinit var stepsCountText: TextView
     private lateinit var stepsProgressBar : ProgressBar
+    private lateinit var viewModel: ActivitiesViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        db = BrockDB.getInstance(requireContext())
-        val factoryViewModelActivities = ActivitiesViewModelFactory(db)
-
-        val recyclerView = view.findViewById<RecyclerView>(R.id.home_recycler_view)
-
         stepsProgressBar = view.findViewById(R.id.steps_progress_bar)
         stepsCountText = view.findViewById(R.id.steps_count_text)
 
-        val viewModel = ViewModelProvider(this, factoryViewModelActivities)[ActivitiesViewModel::class.java]
+        val spinner = view.findViewById<Spinner>(R.id.home_spinner)
+        setUpSpinner(spinner)
 
         user = User.getInstance()
 
-        viewModel.getWeekUserActivities(LocalDate.now(), user)
-        viewModel.getDayUserActivities(LocalDate.now().toString(), user)
+        val db = BrockDB.getInstance(requireContext())
+        val factoryViewModelActivities = ActivitiesViewModelFactory(db)
 
-        viewModel.sortedWeekActivitiesDayList.observe(viewLifecycleOwner) { item ->
-            if (item.isNotEmpty()) {
-                populateHomeRecyclerView(recyclerView, item)
+        viewModel = ViewModelProvider(this, factoryViewModelActivities)[ActivitiesViewModel::class.java]
 
-                CoroutineScope(Dispatchers.IO).launch {
-                    populateStepProgressBar()
+        observeUserActivities()
+        observeUserSteps()
+    }
+
+    private fun setUpSpinner(spinner: Spinner?) {
+        val spinnerItems = resources.getStringArray(R.array.spinner_items)
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinnerItems)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner?.adapter = adapter
+
+        spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = spinnerItems[position]
+                val range = when (selectedItem) {
+                    "Giorno" -> {
+                        getDayRange(LocalDate.now())
+                    }
+
+                    "Settimana" -> {
+                        getWeekRange(LocalDate.now())
+                    }
+
+                    else -> {
+                        getDayRange(LocalDate.now())
+                    }
                 }
-            }
-        }
 
-        viewModel.sortedDayExitActivitiesList.observe(viewLifecycleOwner) { item ->
-            val userWalkActivities = item.filter { it.type == WALK_ACTIVITY_TYPE}
-
-            val steps = userWalkActivities.parallelStream().mapToInt { it.info.toInt() }.sum()
-            CoroutineScope(Dispatchers.Main).launch {
-                updateSteps(steps)
+                viewModel.getUserActivities(range.first, range.second, user)
             }
 
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                //
+            }
         }
     }
 
-    private suspend fun populateStepProgressBar() {
-        val steps = db.UserWalkActivityDao().getEndingWalkActivitiesByUserIdAndPeriod(user.id,
-            LocalDate.now().atTime(0,0,0).toString(), LocalDate.now().atTime(23,59,59).toString()).parallelStream().mapToInt { it.stepNumber.toInt() }.sum()
+    private fun observeUserActivities() {
+        viewModel.listActivities.observe(viewLifecycleOwner) { listActivities ->
+            if (listActivities.isNotEmpty()) {
+                val recyclerView = view?.findViewById<RecyclerView>(R.id.home_recycler_view)
+                populateHomeRecyclerView(recyclerView, listActivities)
 
-        updateSteps(steps)
+                viewModel.getSteps(user)
+            } else {
+                Log.d("HOME_FRAGMENT", "None activities.")
+            }
+        }
     }
 
-    private fun populateHomeRecyclerView(homeList: RecyclerView, activities: List<UserActivity>) {
+    private fun observeUserSteps() {
+        viewModel.steps.observe(viewLifecycleOwner) { steps ->
+            if (steps > 0) {
+                stepsProgressBar.progress = steps
+                stepsCountText.text = "$steps/10000 passi"
+            } else {
+                Log.d("HOME_FRAGMENT", "None steps detected.")
+            }
+        }
+    }
+
+    private fun populateHomeRecyclerView(recyclerView: RecyclerView?, activities: List<UserActivity>) {
         val adapterHome = HomeAdapter(activities)
         val layoutManager = LinearLayoutManager(requireContext())
 
-        homeList.adapter = adapterHome
-        homeList.layoutManager = layoutManager
+        recyclerView?.adapter = adapterHome
+        recyclerView?.layoutManager = layoutManager
     }
 
-    private fun updateSteps(steps: Int) {
-        stepsProgressBar.progress = steps
-        stepsCountText.text = "$steps/10000 passi"
+    private fun getDayRange(day: LocalDate): Pair<String, String> {
+        val startOfDay = day.atStartOfDay().withSecond(0)
+        val endOfDay = startOfDay.plusDays(1).minusSeconds(1)
+        val outputFormatter = DateTimeFormatter.ofPattern(ISO_DATE_FORMAT)
+
+        return Pair(startOfDay.format(outputFormatter), endOfDay.format(outputFormatter))
+    }
+
+    private fun getWeekRange(day: LocalDate): Pair<String, String> {
+        val firstDay = day.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)).atStartOfDay().withSecond(0)
+        val lastDay = day.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY)).atStartOfDay().plusDays(1).minusSeconds(1)
+        val outputFormatter = DateTimeFormatter.ofPattern(ISO_DATE_FORMAT)
+
+        return Pair(firstDay.format(outputFormatter), lastDay.format(outputFormatter))
     }
 }
--- app/src/main/java/com/example/brockapp/viewmodel/ActivitiesViewModel.kt
+++ app/src/main/java/com/example/brockapp/viewmodel/ActivitiesViewModel.kt
@@ -23,8 +23,58 @@ class ActivitiesViewModel(private val db: BrockDB): ViewModel() {
     private val _sortedDayActivitiesList = MutableLiveData<List<UserActivity>>()
     val sortedDayActivitiesList: LiveData<List<UserActivity>> get() = _sortedDayActivitiesList
 
-    private val _sortedWeekActivitiesList = MutableLiveData<List<UserActivity>>()
-    val sortedWeekActivitiesDayList: LiveData<List<UserActivity>> get() = _sortedWeekActivitiesList
+    private val _listActivities = MutableLiveData<List<UserActivity>>()
+    val listActivities: LiveData<List<UserActivity>> get() = _listActivities
+
+    private val _steps = MutableLiveData<Int>()
+    val steps: LiveData<Int> get() = _steps
+
+    fun getUserActivities(startOfDay: String, endOfDay: String, user: User) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val listActivities = ArrayList<UserActivity>()
+
+            val listStillActivities = db.UserStillActivityDao().getStillActivitiesByUserIdAndPeriod(user.id, startOfDay, endOfDay)
+            val listVehicleActivities = db.UserVehicleActivityDao().getVehicleActivitiesByUserIdAndPeriod(user.id, startOfDay, endOfDay)
+            val listWalkingActivities = db.UserWalkActivityDao().getEndingWalkActivitiesByUserIdAndPeriod(user.id, startOfDay, endOfDay)
+
+            listStillActivities.parallelStream().forEach {
+                val newActivity = UserActivity(it.id, it.userId, it.timestamp, it.transitionType, STILL_ACTIVITY_TYPE, "METTERE DURATA STILL")
+                listActivities.add(newActivity)
+            }
+
+            listVehicleActivities.parallelStream().forEach {
+                val newActivity = UserActivity(it.id, it.userId, it.timestamp, it.transitionType, VEHICLE_ACTIVITY_TYPE, it.distanceTravelled.toString())
+                listActivities.add(newActivity)
+            }
+
+            listWalkingActivities.parallelStream().forEach {
+                val newActivity = UserActivity(it.id, it.userId, it.timestamp, it.transitionType, WALK_ACTIVITY_TYPE, it.stepNumber.toString())
+                listActivities.add(newActivity)
+            }
+
+            val listExitActivities = listActivities
+                .filter {
+                    it.transitionType == 1
+                }
+                .sortedBy {
+                    it.timestamp
+                }
+
+            _listActivities.postValue(listExitActivities)
+        }
+    }
+
+    fun getSteps(user: User) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val steps = db.UserWalkActivityDao().getEndingWalkActivitiesByUserIdAndPeriod(
+                user.id,
+                LocalDate.now().atTime(0, 0, 0).toString(),
+                LocalDate.now().atTime(23, 59, 59).toString()
+            ).parallelStream().mapToInt { it.stepNumber.toInt() }.sum()
+
+            _steps.postValue(steps)
+        }
+    }
 
     fun getDayUserActivities(date: String?, user: User) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -56,34 +106,6 @@ class ActivitiesViewModel(private val db: BrockDB): ViewModel() {
         }
     }
 
-    fun getWeekUserActivities(day: LocalDate, user: User) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val (startOfWeek, endOfWeek) = getWeekRange(day)
-            val listActivities = mutableListOf<UserActivity>()
-
-            val listStillActivities = db.UserStillActivityDao().getEndingStillActivitiesByUserIdAndPeriod(user.id, startOfWeek, endOfWeek)
-            val listVehicleActivities = db.UserVehicleActivityDao().getEndingVehicleActivitiesByUserIdAndPeriod(user.id, startOfWeek, endOfWeek)
-            val listWalkingActivities = db.UserWalkActivityDao().getEndingWalkActivitiesByUserIdAndPeriod(user.id, startOfWeek, endOfWeek)
-
-            listStillActivities.parallelStream().forEach {
-                val newActivity = UserActivity(it.id, it.userId, it.timestamp, it.transitionType, STILL_ACTIVITY_TYPE, "METTERE DURATA STILL")
-                listActivities.add(newActivity)
-            }
-
-            listVehicleActivities.parallelStream().forEach {
-                val newActivity = UserActivity(it.id, it.userId, it.timestamp, it.transitionType, VEHICLE_ACTIVITY_TYPE, it.distanceTravelled.toString())
-                listActivities.add(newActivity)
-            }
-
-            listWalkingActivities.parallelStream().forEach {
-                val newActivity = UserActivity(it.id, it.userId, it.timestamp, it.transitionType, WALK_ACTIVITY_TYPE, it.stepNumber.toString())
-                listActivities.add(newActivity)
-            }
-
-            _sortedWeekActivitiesList.postValue(listActivities.sortedBy { it.timestamp })
-        }
-    }
-
     private fun getDayRange(dateStr: String?): Pair<String, String> {
         val date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
 
@@ -93,14 +115,5 @@ class ActivitiesViewModel(private val db: BrockDB): ViewModel() {
         val outputFormatter = DateTimeFormatter.ofPattern(ISO_DATE_FORMAT)
 
         return Pair(startOfDay.format(outputFormatter), endOfDay.format(outputFormatter))
-    }
-
-    private fun getWeekRange(currentDay: LocalDate): Pair<String, String> {
-        val firstDay = currentDay.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)).atStartOfDay().withSecond(0)
-        val lastDay = currentDay.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY)).atStartOfDay().plusDays(1).minusSeconds(1)
-
-        val outputFormatter = DateTimeFormatter.ofPattern(ISO_DATE_FORMAT)
-
-        return Pair(firstDay.format(outputFormatter), lastDay.format(outputFormatter))
     }
 }
