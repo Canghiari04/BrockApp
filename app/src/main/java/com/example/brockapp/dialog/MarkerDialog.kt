adf2d3020119e40d5152a3b0b8070ca00196bc4e
Canghiari04 <matteo.canghiari@gmail.com>
2024-10-07 21:50:34 +0200

    modifica comportamentale del dialog

--- app/src/main/java/com/example/brockapp/dialog/MarkerDialog.kt
+++ app/src/main/java/com/example/brockapp/dialog/MarkerDialog.kt
@@ -3,12 +3,13 @@ package com.example.brockapp.dialog
 import com.example.brockapp.R
 import com.example.brockapp.viewmodel.GeofenceViewModel
 
-import android.view.View
+import java.util.Locale
 import android.os.Bundle
+import android.view.View
 import android.view.Gravity
-import android.widget.Button
 import android.view.ViewGroup
 import android.widget.TextView
+import android.location.Geocoder
 import android.view.LayoutInflater
 import androidx.fragment.app.DialogFragment
 import com.google.android.gms.maps.model.Marker
@@ -29,26 +30,31 @@ class MarkerDialog(private val marker: Marker, private val viewModel: GeofenceVi
         val longitude = marker.position.longitude
         val latitude = marker.position.latitude
 
-        val textViewLocality = view.findViewById<TextView>(R.id.text_locality_marker)
-        val textViewLongitude = view.findViewById<TextView>(R.id.text_longitude_marker)
-        val textViewLatitude = view.findViewById<TextView>(R.id.text_latitude_marker)
+        val textViewLocality = view.findViewById<TextView>(R.id.text_view_locality_marker)
+        val textViewAddress = view.findViewById<TextView>(R.id.text_view_address_marker)
 
-        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         dialog?.window?.setGravity(Gravity.CENTER)
         dialog?.setCanceledOnTouchOutside(true)
 
         textViewLocality.setText(name)
-        textViewLongitude.setText(longitude.toString())
-        textViewLatitude.setText(latitude.toString())
+        textViewAddress.setText(getAddress(latitude, longitude))
 
-        view.findViewById<Button>(R.id.delete_marker_button).setOnClickListener {
+        view.findViewById<TextView>(R.id.delete_marker_button).setOnClickListener {
             viewModel.deleteGeofenceArea(name, longitude, latitude)
             marker.remove()
             dismiss()
         }
+    }
 
-        view.findViewById<Button>(R.id.dismiss_dialog_button).setOnClickListener {
-            dismiss()
+    private fun getAddress(latitude: Double, longitude: Double): String? {
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+        val address = geocoder.getFromLocation(latitude, longitude, 1)
+
+        return if (!address.isNullOrEmpty()) {
+            "${address[0].thoroughfare}, ${address[0].locality}"
+        } else {
+            null
         }
     }
 }
