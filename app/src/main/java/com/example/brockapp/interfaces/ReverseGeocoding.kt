39e6d5407db97653733a5afbb881f4cf47fc990e
Canghiari04 <matteo.canghiari@gmail.com>
2024-11-04 12:11:45 +0100

    realizzata interfaccia per estrapolare dal geocoder info sulle località

--- app/src/main/java/com/example/brockapp/interfaces/ReverseGeocoding.kt
+++ app/src/main/java/com/example/brockapp/interfaces/ReverseGeocoding.kt
@@ -1,4 +1,9 @@
 package com.example.brockapp.interfaces
 
+import android.location.Address
+
 interface ReverseGeocoding {
+    fun getAddress(name: String?, latitude: Double, longitude: Double): Address
+
+    suspend fun getGeofenceName(latitude: Double, longitude: Double): String?
 }
