96c650ef8e3363b8f51dc2377794be026d315a18
Canghiari04 <matteo.canghiari@gmail.com>
2024-11-04 12:11:45 +0100

    realizzata interfaccia per calcolo della cadenza della settimana

--- app/src/main/java/com/example/brockapp/interfaces/ReverseGeocodingImpl.kt
+++ app/src/main/java/com/example/brockapp/interfaces/ReverseGeocodingImpl.kt
@@ -1,4 +1,55 @@
 package com.example.brockapp.interfaces
 
-class ReverseGeocondingImpl {
+import com.example.brockapp.room.BrockDB
+import com.example.brockapp.extraObject.MyUser
+
+import java.util.Locale
+import android.content.Context
+import android.location.Address
+import kotlinx.coroutines.async
+import android.location.Geocoder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineScope
+
+class ReverseGeocodingImpl(private val context: Context): ReverseGeocoding {
+    override fun getAddress(name: String?, latitude: Double, longitude: Double): Address {
+        val geocoder = Geocoder(context, Locale.getDefault())
+        val addresses = geocoder.getFromLocation(latitude, longitude, 10)
+
+        if (!name.isNullOrEmpty() && !addresses.isNullOrEmpty()) {
+            for (address in addresses) {
+                if (address.featureName.equals(name)) return address
+            }
+        }
+
+        return addresses!![0]
+    }
+
+    override suspend fun getGeofenceName(latitude: Double, longitude: Double): String? {
+        val db = BrockDB.getInstance(context)
+        val geocoder = Geocoder(context, Locale.getDefault())
+
+        return try {
+            var geofenceName: String? = null
+            val addresses = geocoder.getFromLocation(latitude, longitude, 10)
+
+            if (!addresses.isNullOrEmpty()) {
+                val items = CoroutineScope(Dispatchers.IO).async {
+                    db.GeofenceAreasDao().getAllGeofenceAreasByUsername(MyUser.username).map { it.name }
+                }.await()
+
+                if (items.isNotEmpty()) {
+                    addresses.forEach {
+                        if (items.contains(it.featureName)) {
+                            geofenceName = it.featureName
+                        }
+                    }
+                }
+            }
+
+            geofenceName
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
