d26783c9a6189b681a349837f07bf650c51f1370
Canghiari04 <matteo.canghiari@gmail.com>
2024-11-04 12:34:59 +0100

    attuata interfaccia InternetAvailable, adeguato intent alla enum class del service

--- app/src/main/java/com/example/brockapp/receiver/AuthenticatorReceiver.kt
+++ app/src/main/java/com/example/brockapp/receiver/AuthenticatorReceiver.kt
@@ -1,39 +1,47 @@
 package com.example.brockapp.receiver
 
+import com.example.brockapp.worker.ConnectivityWorker
 import com.example.brockapp.viewmodel.NetworkViewModel
+import com.example.brockapp.interfaces.InternetAvailableImpl
 
+import androidx.work.Data
 import android.content.Intent
 import android.content.Context
+import androidx.work.WorkManager
 import android.net.ConnectivityManager
 import android.content.BroadcastReceiver
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
+import androidx.work.OneTimeWorkRequestBuilder
 
 private var lastHandledTime = 0L
 private const val DEBOUNCE_INTERVAL = 5000L
 
 class AuthenticatorReceiver(private val viewModelStoreOwner: ViewModelStoreOwner): BroadcastReceiver() {
+    private var networkUtil = InternetAvailableImpl()
+
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == ConnectivityManager.CONNECTIVITY_ACTION) {
-            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val networkInfo = connectivityManager.activeNetworkInfo
-
             val currentTime = System.currentTimeMillis()
             if (currentTime - lastHandledTime < DEBOUNCE_INTERVAL) {
                 return
             }
             lastHandledTime = currentTime
 
-            if (networkInfo != null && networkInfo.isConnected) {
-                handleChange(true)
-            } else {
-                handleChange(false)
-            }
-        }
-    }
+            val isConnected = networkUtil.isInternetActive(context)
 
-    private fun handleChange(isConnected: Boolean) {
-        val networkViewModel = ViewModelProvider(viewModelStoreOwner)[NetworkViewModel::class.java]
-        networkViewModel.setAuthNetwork(isConnected)
+            ViewModelProvider(viewModelStoreOwner)[NetworkViewModel::class.java].also {
+                it.setAuthNetwork(isConnected)
+            }
+
+            WorkManager.getInstance(context).enqueue(
+                OneTimeWorkRequestBuilder<ConnectivityWorker>()
+                    .setInputData(
+                        Data.Builder()
+                            .putBoolean("IS_CONNECTED", isConnected)
+                            .build()
+                    ).build()
+            )
+        }
     }
 }
