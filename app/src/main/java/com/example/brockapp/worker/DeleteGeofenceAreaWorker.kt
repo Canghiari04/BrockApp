9985d74652519632e90e105795014213d3888d35
Canghiari04 <matteo.canghiari@gmail.com>
2024-11-08 20:14:14 +0100

    realizzato worker per eliminare geofence all'interno di supabase

--- app/src/main/java/com/example/brockapp/worker/DeleteGeofenceAreaWorker.kt
+++ app/src/main/java/com/example/brockapp/worker/DeleteGeofenceAreaWorker.kt
@@ -1,4 +1,26 @@
 package com.example.brockapp.worker
 
-class DeleteGeofenceAreaWorker {
+import com.example.brockapp.extraObject.MyUser
+import com.example.brockapp.singleton.MySupabase
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import io.github.jan.supabase.postgrest.from
+
+class DeleteGeofenceAreaWorker(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        val latitude = inputData.getDouble("LATITUDE", 0.0)
+        val longitude = inputData.getDouble("LONGITUDE", 0.0)
+
+        if (latitude != 0.0 && longitude != 0.0) {
+            MySupabase.getInstance().from("GeofenceAreas").delete {
+                filter { eq("latitude", latitude) }
+                filter { eq("longitude", longitude) }
+                filter { eq("username", MyUser.username) }
+            }
+        }
+
+        return Result.success()
+    }
 }
