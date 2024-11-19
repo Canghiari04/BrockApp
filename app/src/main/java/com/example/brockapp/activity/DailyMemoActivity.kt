0f4e8eed1f988b29d352f9326315e7cfe24c03eb
Canghiari04 <matteo.canghiari@gmail.com>
2024-11-19 16:56:25 +0100

    realizzato nuovo layout per la empty page tramite composable

--- app/src/main/java/com/example/brockapp/activity/DailyMemoActivity.kt
+++ app/src/main/java/com/example/brockapp/activity/DailyMemoActivity.kt
@@ -4,44 +4,64 @@ import com.example.brockapp.*
 import com.example.brockapp.R
 import com.example.brockapp.room.BrockDB
 import com.example.brockapp.room.MemosEntity
-import com.example.brockapp.viewmodel.MemoViewModel
+import com.example.brockapp.viewModel.MemoViewModel
+import com.example.brockapp.util.ScheduleWorkerUtil
 import com.example.brockapp.adapter.DailyMemoAdapter
-import com.example.brockapp.interfaces.ScheduleWorkerImpl
-import com.example.brockapp.viewmodel.MemoViewModelFactory
+import com.example.brockapp.viewModel.MemoViewModelFactory
 
+import java.util.Locale
 import android.util.Log
 import android.os.Bundle
 import java.time.LocalDate
-import android.view.MenuItem
 import android.content.Intent
-import androidx.appcompat.widget.Toolbar
+import java.time.format.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
 import java.time.format.DateTimeFormatter
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModelProvider
+import androidx.compose.material.icons.Icons
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.material3.MaterialTheme
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.ComposeView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.compose.material.icons.outlined.Warning
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DailyMemoActivity: AppCompatActivity() {
+
     private var date: String? = null
 
     private lateinit var viewModel: MemoViewModel
     private lateinit var adapter: DailyMemoAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var button: FloatingActionButton
-    private lateinit var scheduleWorkerUtil: ScheduleWorkerImpl
+    private lateinit var scheduleWorkerUtil: ScheduleWorkerUtil
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily_memo)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar_daily_memo_activity)
-        setSupportActionBar(toolbar)
+        date = intent.getStringExtra("CALENDAR_DATE")
+
+        val item = LocalDate.parse(
+            date,
+            DateTimeFormatter.ofPattern(CALENDAR_DATE_FORMAT)
+        )
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = resources.getText(R.string.toolbar_daily_memo)
-
-        date = intent.getStringExtra("CALENDAR_DATE")
+        supportActionBar?.title = "${item.dayOfWeek.getDisplayName(TextStyle.FULL, Locale.US)}, ${item.month.toString().lowercase().replaceFirstChar { it.uppercase() }} ${item.dayOfMonth}"
 
         val db = BrockDB.getInstance(this)
         val viewModelFactory = MemoViewModelFactory(db)
@@ -52,30 +72,11 @@ class DailyMemoActivity: AppCompatActivity() {
         button = findViewById(R.id.button_new_memo)
         setUpFloatingButton(date)
 
-        scheduleWorkerUtil = ScheduleWorkerImpl(this)
+        scheduleWorkerUtil = ScheduleWorkerUtil(this)
 
         observeMemos()
 
         viewModel.getMemos(date!!)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                val intent = Intent(this, PageLoaderActivity::class.java).putExtra(
-                    "FRAGMENT_TO_SHOW",
-                    R.id.navbar_item_calendar
-                )
-                startActivity(intent)
-                finish()
-                true
-            }
-
-            else -> {
-                super.onOptionsItemSelected(item)
-                false
-            }
-        }
     }
 
     override fun onPause() {
@@ -93,7 +94,7 @@ class DailyMemoActivity: AppCompatActivity() {
 
     private fun setUpFloatingButton(date: String?) {
         button.setOnClickListener {
-            val intent = Intent(this, NewMemoActivity::class.java).putExtra("CALENDAR_DATE", date)
+            val intent = Intent(this, RegistrationMemoActivity::class.java).putExtra("CALENDAR_DATE", date)
             startActivity(intent)
             finish()
         }
@@ -109,7 +110,7 @@ class DailyMemoActivity: AppCompatActivity() {
 
         return currentDate.isEqual(dailyDate) || currentDate.isBefore(dailyDate)
     }
-    
+
     private fun observeMemos() {
         viewModel.memos.observe(this) { list ->
             if (!list.isNullOrEmpty()) {
@@ -118,10 +119,12 @@ class DailyMemoActivity: AppCompatActivity() {
                 if (!checkCurrentDate(date)) {
                     setContentView(R.layout.activity_empty_page)
 
-                    val toolbar = findViewById<Toolbar>(R.id.toolbar_empty_activity)
-                    setSupportActionBar(toolbar)
+                    val composeView = findViewById<ComposeView>(R.id.compose_view_activity_empty_page)
 
-                    supportActionBar?.title = null
+                    composeView.setContent {
+                        EmptyPage()
+                    }
+
                     supportActionBar?.setDisplayHomeAsUpEnabled(true)
                 }
             }
@@ -134,5 +137,32 @@ class DailyMemoActivity: AppCompatActivity() {
 
         recyclerView.adapter = adapter
         recyclerView.layoutManager = layoutManager
+    }
+
+    @Composable
+    fun EmptyPage() {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Warning,
+                contentDescription = "Empty",
+                modifier = Modifier
+                    .size(48.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally),
+                tint = Color.Gray
+            )
+            Text(
+                text = "No memo found",
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    fontSize = 18.sp,
+                    color = Color.Gray,
+                ),
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
